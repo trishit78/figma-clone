6cc6df102d7dd55adec60c53d87212719f1b1b24
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-08-08 21:40:24 +0530

    Feat: signin page done

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,13 +1,32 @@
 "use server"
 
 import bcrypt from "bcryptjs";
+import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
 //import { redirect } from "next/dist/server/api-utils";
 import { isValid, ZodError } from "zod";
 import { signUpSchema } from "~/schemas";
+import { signIn } from "~/server/auth";
 import { db } from "~/server/db";
 
-
+export async function authenticate(prevState:string| undefined, formData:FormData) {
+    try {
+        
+        await signIn("credentials",formData)
+        
+    } catch (error) {
+         if (error instanceof AuthError){
+           
+            if (error.type == "CredentialsSignin"){
+                return "Invalid credentials"
+            }
+            else{
+                       return "Something went wrong"
+            }
+    }
+    throw error;
+    }
+}
 
 export async function register(prevState:string| undefined, formData:FormData) {
  try {
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,11 +2,11 @@
 
 import Link from "next/link";
 import React, { useActionState } from "react";
-import { register } from "../actions/auth";
+import { authenticate } from "../actions/auth";
 
 const Page = () => {
   const [errorMessage, formAction, isPending] = useActionState(
-    register,
+    authenticate,
     undefined,
   );
 
@@ -14,10 +14,13 @@ const Page = () => {
     <div className="flex min-h-screen items-center justify-center bg-white px-4">
       <div className="w-full max-w-sm space-y-6">
         <h1 className="text-center text-2xl font-semibold text-gray-900">
-          Sign Up
+          Sign In
         </h1>
         <form action={formAction} className="space-y-4">
+          <input type="hidden" name="redirectTo" value="/dashboard" />
+          
           <div className="relative h-fit">
+
             <input
               className="w-full rounded-md border border-gray-300 px-3 pt-7 pb-1 text-sm focus:border-black focus:outline-none"
               type="email"
@@ -40,16 +43,16 @@ const Page = () => {
           </div>
 
           <button disabled={isPending} className="w-full rounded-md bg-black py-2 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-300">
-            {isPending ? "Registering...":"Register"}
+            {isPending ? "Logging in...":"Log in"}
           </button>
 
           <p className="text-center text-xs text-gray-600">
-            Have an account?{" "}
+            No account?{" "}
             <Link
               className="text-blue-400 hover:text-blue-600"
-              href={"/signin"}
+              href={"/signup"}
             >
-              Register
+              Create One
             </Link>
           </p>
 
